4f07886351a998f5e2e2944fb2631ca6b6c0caca
rishita-ops <rishita.sanghavi07@gmail.com>
2026-03-16 23:59:27 +0530

    First Commit

---
+++ code.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of the array: ";
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    cout<<"The reversed array is: ";
+    for(int i=n-1; i>=0; i--)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
